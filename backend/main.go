19df50e0bc3e62b0c2f5ca699bf14a743c7c3d8d
Erkin Dilekci <dilekcierkin@gmail.com>
2024-08-29 09:04:10 +0300

    Use environment variables

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"ims-intro/models"
 	"ims-intro/routers"
 	"log"
+	"os"
 )
 
 func main() {
-	dataSourceName := "user=postgres port=5434 password=password dbname=ims sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPort := os.Getenv("DB_PORT")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	dataSourceName := "user=" + dbUser + " port=" + dbPort + " password=" + dbPassword + " dbname=" + dbName + " sslmode=" + dbSSLMode
 	models.InitDB(dataSourceName)
 
 	e := routers.SetupRouter()
 
-	log.Println("Server is running on port 8080")
-	e.Logger.Fatal(e.Start(":8080"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server is running on port", port)
+	e.Logger.Fatal(e.Start(":" + port))
 }
